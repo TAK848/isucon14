bea16e7c0df5255c9c58df8da2c7d2a3f4cfb704
Takuya Takahashi <pairs_char0j@icloud.com>
2024-12-08 15:10:47 +0900

    SQL観光名所爆破

--- go/models.go
+++ go/models.go
@@ -6,14 +6,18 @@ import (
 )
 
 type Chair struct {
-	ID          string    `db:"id"`
-	OwnerID     string    `db:"owner_id"`
-	Name        string    `db:"name"`
-	Model       string    `db:"model"`
-	IsActive    bool      `db:"is_active"`
-	AccessToken string    `db:"access_token"`
-	CreatedAt   time.Time `db:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at"`
+	ID                     string     `db:"id"`
+	OwnerID                string     `db:"owner_id"`
+	Name                   string     `db:"name"`
+	Model                  string     `db:"model"`
+	IsActive               bool       `db:"is_active"`
+	AccessToken            string     `db:"access_token"`
+	Longitude              *int       `db:"longitude"`
+	Latitude               *int       `db:"latitude"`
+	TotalDistance          int        `db:"total_distance"`
+	TotalDistanceUpdatedAt *time.Time `db:"total_distance_updated_at"`
+	CreatedAt              time.Time  `db:"created_at"`
+	UpdatedAt              time.Time  `db:"updated_at"`
 }
 
 type ChairModel struct {
